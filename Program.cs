7092d6477d689952bd0b08d6783edc083e3072a0
Piotr <piotrsitkowski@wp.pl>
2022-04-23 14:33:19 +0200

    Add checking the number

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var selectedNumber = GetNumber();
+
+
+        }
+
+        private static int GetNumber()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("Musisz podać Liczbę.");
+                    continue;
+                }
+                return number;
+            }
+        }
+    }
+}
